5a66d9a4c8d8b6124b95547a20415803346331ad
lucasrgsp <lucassasuke2000@gmail.com>
2019-02-21 10:46:00 -0300

    atividades python

---
+++ maximo de uma lista de números.py
@@ -0,0 +1,28 @@
+ def main():
+    ''' teste da funcao maximo '''
+     minha_lista = [12,34,78,90]
+     maior_numero, indice = max_lista(minha lista)
+     print("o maior elemento é:" maior_numero)
+     print("O indice é =", indice)
+  
+
+
+
+ def max_lista(lista):
+    '''(list) -> int, int
+    recebe uma lista de inteiros e retorna o
+    valor maximo da lista e o indice onde ele ocorre'''
+
+    maior = -25
+     
+     for n in lista:
+
+        print(n)
+
+
+
+    return 0, 0
+
+main()
+
+
